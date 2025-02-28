8f2e2940132068ab67195eedaff077636f2680cb
Cids Ho <hocids@tongji.edu.cn>
2025-03-01 01:38:36 +0800

    update blog

--- scripts/script.js
+++ scripts/script.js
@@ -421,3 +421,13 @@ languageButtons.forEach(button => {
 
 // 存储语言偏好
 localStorage.setItem('preferredLanguage', lang);}
+
+// 返回按钮逻辑
+const backButton = document.getElementById('back-button');
+if (backButton) {
+    backButton.addEventListener('click', () => {
+        window.history.back(); // 返回上一个页面
+    });
+} else {
+    console.error('返回按钮未找到');
+}
